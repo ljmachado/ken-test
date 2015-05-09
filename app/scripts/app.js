f7df93b482cda9385bf6f310339f467812c5ec27
Lucas Vieira <vieiralucas4@gmail.com>
2015-05-09 14:59:18 -0300

    Much better menu screen

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -7,40 +7,43 @@ angular
         'angular-storage'
     ])
     .config(function($httpProvider, $urlRouterProvider, $stateProvider) {
-        $urlRouterProvider.otherwise("/")
+        $urlRouterProvider
+            .when('/menu', '/menu/cadastre')
+            .otherwise("/")
+
         $stateProvider
-          .state('login', {
-              url: '/',
-              templateUrl: 'views/login.html',
-              controller: 'LoginController',
-              data: {
-                  requireLogin: false
-              }
-          })
-          .state('menu', {
-              url: '/menu',
-              templateUrl: 'views/menu.html',
-              controller: 'MenuController',
-              data: {
+            .state('login', {
+                url: '/',
+                templateUrl: 'views/login.html',
+                controller: 'LoginController',
+                data: {
+                    requireLogin: false
+                }
+            })
+            .state('menu', {
+                url: '/menu',
+                templateUrl: 'views/menu.html',
+                controller: 'MenuController',
+                data: {
+                    requireLogin: true
+                }
+            })
+            .state('menu.cadastre', {
+                url: '/cadastre',
+                templateUrl: 'views/cadastre.html',
+                controller: 'CadastreController',
+                data: {
                   requireLogin: true
-              }
-          })
-          .state('cadastre', {
-              url: '/cadastre',
-              templateUrl: 'views/cadastre.html',
-              controller: 'CadastreController',
-              data: {
-                requireLogin: true
-              }
-          })
-          .state('identify', {
-              url: '/identify',
-              templateUrl: 'views/identify.html',
-              controller: 'IdentifyController',
-              data: {
-                requireLogin: true
-              }
-          });
+                }
+            })
+            .state('menu.identify', {
+                url: '/identify',
+                templateUrl: 'views/identify.html',
+                controller: 'IdentifyController',
+                data: {
+                  requireLogin: true
+                }
+            });
     })
     .run(['$rootScope', '$state', 'User', function($rootScope, $state, $user) {
         $rootScope.currentUser = null;
